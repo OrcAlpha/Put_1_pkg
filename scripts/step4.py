9b6f8cb905a0a298805dab805714a0de92eaf88b
OrcAlpha <orcinus.orca.take@gmail.com>
2022-01-13 10:24:57 +0000

    Add broken step4

---
+++ scripts/step4.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import Int32
+
+n = 0
+ini = 0
+
+def cb1(message):
+    global n
+    n = message.data/2
+
+def cb2(message):
+    global ini
+    ini = message.data
+
+rospy.init_node('step4')
+sub1 = rospy.Subscriber('step3', Int32, cb1)
+sub2 = rospy.Subscriber('step3', Int32, cb2)
+pub1 = rospy.Publisher('step4', Int32, queue_size=1)
+pub2 = rospy.Publisher('step4', Int32, queue_size=1)
+rate = rospy.Rate(10)
+while not rospy.is_shutdown():
+    pub1.publish(n)
+    pub2.publish(ini)
+    rate.sleep()
