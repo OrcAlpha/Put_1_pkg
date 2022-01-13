eac5ca5dc78517213b48a46379da2f0ca134aa2e
OrcAlpha <orcinus.orca.take@gmail.com>
2022-01-13 10:11:33 +0000

    Add step3

--- scripts/step3.py
+++ scripts/step3.py
@@ -3,16 +3,23 @@ import rospy
 from std_msgs.msg import Int32
 
 n = 0
+ini = 0
 
-def cb(message):
+def cb1(message):
     global n
     n = message.data-6
+
+def cb2(message):
+    global ini
     ini = message.data
 
 rospy.init_node('step3')
-sub = rospy.Subscriber('step2', Int32, cb)
-pub = rospy.Publisher('step3', Int32, queue_size=1)
+sub1 = rospy.Subscriber('step2', Int32, cb1)
+sub2 = rospy.Subscriber('step2', Int32, cb2)
+pub1 = rospy.Publisher('step3', Int32, queue_size=1)
+pub2 = rospy.Publisher('step3', Int32, queue_size=1)
 rate = rospy.Rate(10)
 while not rospy.is_shutdown():
-    pub.publish(n,ini)
+    pub1.publish(n)
+    pub2.publish(ini)
     rate.sleep()
